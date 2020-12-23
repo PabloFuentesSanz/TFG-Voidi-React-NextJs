0d2389ced049d1dc82d8990c0f67e417bdefa792
Pablo Fuentes <fuentessanzpablo@gmail.com>
2020-12-23 22:51:10 +0100

    Cambios en las media queries para hacerlo responsive

--- pages/components/FormHome.js
+++ pages/components/FormHome.js
@@ -1,23 +1,47 @@
 import styles from "../../styles/FormHome.module.css";
 
 export default function Form(props) {
+  let type = props.type;
   let title = "";
   let mail = "";
   let pass = "";
-  if (props.type == "log") {
+  let style = "";
+  if (type == "log") {
     title = "Login";
-    pass = <p>¿Has olvidado tu contraseña?</p>
+    pass = <p>¿Has olvidado tu contraseña?</p>;
+    style= styles.log;
   } else {
     title = "Sign in";
-    mail = <input className={styles.input__field} type="email" name="email" id="" placeholder="email"/>
+    mail = (
+      <input
+        className={styles.input__field}
+        type="email"
+        name="email"
+        id=""
+        placeholder="email"
+      />
+    );
+    style= styles.sign;
   }
   return (
-    <div className={styles.form__container}>
+    <div className={styles.form__container+` `+style}>
       <form action="">
         <h1 className={styles.title}>{title}</h1>
-        <input className={styles.input__field} type="text" name="username" id="" placeholder="Username" />
+        <input
+          className={styles.input__field}
+          type="text"
+          name="username"
+          id=""
+          placeholder="Username"
+        />
         {mail}
-        <input className={styles.input__field} type="password" name="password" id="" placeholder="Password" />
+        <input
+          className={styles.input__field}
+          type="password"
+          name="password"
+          id=""
+          placeholder="Password"
+        />
         <button className={styles.btn}>CONTINUE</button>
         {pass}
       </form>
