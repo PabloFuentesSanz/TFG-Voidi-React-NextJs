333659b568372e0535f729b099f52848b2b789b6
Pablo Fuentes <fuentessanzpablo@gmail.com>
2020-12-28 16:35:39 +0100

    Probando pantalla de carga incial

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,22 @@ import { useEffect, useState, useRef } from "react";
 
 export default function Home() {
   const container = useRef(0);
+  const imagen = useRef(0);
 
-  useEffect(()=>{
+  useEffect(() => {
     container.current.style.opacity = "1";
-  }, [])
+    imagen.current.style.opacity = "0";
+    imagen.current.style.transform = "translate(0, -500%)";
+  }, []);
 
   return (
-    <div className={styles.container} id="container" ref={container} style={{ opacity: '0' }}>
+    <>
+    <div
+      className={styles.container}
+      id="container"
+      ref={container}
+      style={{ opacity: "0" }}
+    >
       <Head>
         <title>Voidi</title>
         <link rel="icon" href="/favicon.png" />
@@ -29,5 +38,20 @@ export default function Home() {
         <Aside type="sign" />
       </div>
     </div>
+      <img
+        src="/logo.png"
+        alt=""
+        ref={imagen}
+        style={{
+          position: "absolute",
+          top: "20%",
+          left: "30%",
+          width: "40%",
+          zIndex: 50,
+          transition: "2s ease-in-out",
+          transitionDelay: "1.5s"
+        }}
+      />
+    </>
   );
 }
