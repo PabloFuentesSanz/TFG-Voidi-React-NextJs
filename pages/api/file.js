e4e1f22b2fa0260d50e81724475f5d4b134c8af6
Pablo <51823158+PabloFuentesSanz@users.noreply.github.com>
2021-05-22 17:30:08 +0200

    Update file.js
    Cirect server side

--- pages/api/file.js
+++ pages/api/file.js
@@ -8,7 +8,7 @@ export const config = {
 
 const post = async (req, res) =>{
   const form = new formidable.IncomingForm();
-  form.uploadDir= 'public/uploads';
+  form.uploadDir= '/uploads';
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) =>{
     console.log(err, fields, files);
