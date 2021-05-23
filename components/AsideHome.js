11977dd8422dc7912a8f23786d238ddc1c79f7e7
pablofuentes@gmail.com <pablofuentessanzfuentes@gmail.com>
2021-05-23 14:57:53 +0200

    Change axios function

--- components/AsideHome.js
+++ components/AsideHome.js
@@ -5,24 +5,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit } from '@fortawesome/free-solid-svg-icons'
 import { updateName, updateImg } from '../firebase';
 import axios from 'axios';
+import { useRef } from "react";
 
 
 export default function AsideHome(props) {
 
     const [name, setName] = useState({ ...props.name })
-    const [image, setImage] = useState(null);
+    const fileInputRef = useRef();
 
     useEffect(() => {
         setName(props.name)
     }, [props.name])
-    
-    
-    const [createObjectURL, setCreateObjectURL] = useState({...props.img});
-    
+
+
+    const [createObjectURL, setCreateObjectURL] = useState({ ...props.img });
+
     useEffect(() => {
         setCreateObjectURL(props.img)
     }, [props.img])
-    
+
     useEffect(() => {
         document.getElementById("imgProfile").style.backgroundImage = "url('" + createObjectURL + "')";
     })
@@ -50,27 +51,32 @@ export default function AsideHome(props) {
     const asideStyle = `${styles.aside} d-none d-sm-block col-sm-3`
 
     const uploadToClient = async (event) => {
-        if (event.target.files && event.target.files[0]) {
-          const i = event.target.files[0];
-    
-          setImage(i);
-          setCreateObjectURL(URL.createObjectURL(i));
-          uploadToServer(i);
-        
+        const files = fileInputRef.current.files;
+        if (fileInputRef.current.files && fileInputRef.current.files[0]) {
+            const body = new FormData();
+
+            for (let index = 0; index <= files.length; index++) {
+                const element = files[index];
+                body.append('file', element)
+            }
+
+            setCreateObjectURL(URL.createObjectURL(files[0]));
+            const response = await axios.post("/api/file", body);
+            updateImg(response.data.url)
+
+
         }
-      };
-    
-      const uploadToServer = async (imagen) => {
+    };
+
+    const uploadToServer = async (imagen) => {
         const body = new FormData();
         body.append("file", imagen);
-        const response = await axios.post("/api/file", body);
-        updateImg(response.data.url)
-      };
+    };
 
     return (
         <aside className={asideStyle}>
             <form action="" >
-                <input className={styles.file} type="file" id="imgUpload" accept="image/png, image/jpg, image/jpeg" onChange={uploadToClient}/>
+                <input className={styles.file} type="file" ref={fileInputRef} id="imgUpload" accept="image/png, image/jpg, image/jpeg" onChange={uploadToClient} />
             </form>
             <div className={styles.image} onClick={handleImg} id="imgProfile" >
 
